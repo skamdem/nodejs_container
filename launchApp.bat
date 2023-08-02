b9d986b82d0e7d1d75a5938f11db61360e646d30
Sylvain K <sylvain.kamdem@gmail.com>
2023-08-02 18:56:58 +0100

    updated image name in "launchApp.*" scripts

--- launchApp.bat
+++ launchApp.bat
@@ -1,5 +1,5 @@
 :: run [ $ launchApp.bat ] to execute this script in command promt
-docker build -t websurfer21/nodecontainer . 
-docker push websurfer21/nodecontainer
-docker run --name nodecontainer --rm -it -p 8084:80 websurfer21/nodecontainer
-docker rmi websurfer21/nodecontainer
+docker build -t websurfer21/nodeimage . 
+docker push websurfer21/nodeimage
+docker run --name nodecontainer --rm -it -p 8084:80 websurfer21/nodeimage
+docker rmi websurfer21/nodeimage
--- launchApp.sh
+++ launchApp.sh
@@ -3,8 +3,8 @@ set -exuo pipefail
 
 # run [ $ ./launchApp.sh ] to execute this script in bash shell
 # clean occasional previous image 
-# docker rmi websurfer21/nodecontainer || echo "image did not exist, proceding..."
-docker build -t websurfer21/nodecontainer . 
-docker push websurfer21/nodecontainer
-docker run --name nodecontainer --rm -it -p 8084:80 websurfer21/nodecontainer
-docker rmi websurfer21/nodecontainer
+# docker rmi websurfer21/nodeimage || echo "image did not exist, proceding..."
+docker build -t websurfer21/nodeimage . 
+docker push websurfer21/nodeimage
+docker run --name nodecontainer --rm -it -p 8084:80 websurfer21/nodeimage
+docker rmi websurfer21/nodeimage
